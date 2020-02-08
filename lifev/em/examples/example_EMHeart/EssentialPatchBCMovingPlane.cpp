c067c49e8c05ab3251ed116d65bbf6d3f3ca3d4d
L G <glaus.luaks@gmail.com>
2020-02-08 12:44:19 +0100

    correction

--- lifev/em/examples/example_EMHeart/EssentialPatchBCMovingPlane.cpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBCMovingPlane.cpp
@@ -295,7 +295,7 @@ vectorPtr_Type EssentialPatchBCMovingPlane::directionalVectorField(EMSolver<Regi
 	                    coordinates(1) = p2PositionVector[jGID];
 	                    coordinates(2) = p2PositionVector[kGID];
 
-                        std::cout << "\n\nIteration number" << j << "of" <<nCompLocalDof-1 << "Coordinate vector" << cordinate(0) << "," << cordinate(1) << "," << cordinate(2) << ",";
+                        std::cout << "\n\nIteration number" << j << "of" <<nCompLocalDof-1 << "Coordinate vector" << coordinates(0) << "," << coordinates(1) << "," << coordinates(2) << ",";
                 
 	                    /*
 	                    coordinates(0) = (*m_p2currentPositionVector)[iGID];
