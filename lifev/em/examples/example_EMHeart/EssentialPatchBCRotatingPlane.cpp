80e04919be74cb0c42df920aae4a1505913164ba
L G <glaus.luaks@gmail.com>
2020-02-11 15:28:49 +0100

    rp 1.0.11

--- lifev/em/examples/example_EMHeart/EssentialPatchBCRotatingPlane.cpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBCRotatingPlane.cpp
@@ -80,7 +80,7 @@ Vector3D calculate_pAxis (const Vector3D pointOnHeart,const Vector3D direction_t
     }
 
 //Calculate the opening angle in function of time
-Real EssentialPatchBC::calculate_angleOfTime (Real maximum_angle, Real minimum_angle, Real& time)
+Real EssentialPatchBCRotatingPlane::calculate_angleOfTime (Real maximum_angle, Real minimum_angle, Real& time)
     {
         Real angleOfTime;
         if (std::fmod(time,m_tduration) <= 0.5)
