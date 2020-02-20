a168bb3ac25676770ce506243d67ff18aa9d1a33
L G <glaus.luaks@gmail.com>
2020-02-20 16:58:22 +0100

    rp 1.6.6

--- lifev/em/examples/example_EMHeart/EssentialPatchBCRotatingPlane.cpp
+++ lifev/em/examples/example_EMHeart/EssentialPatchBCRotatingPlane.cpp
@@ -272,7 +272,8 @@ const bool nodeOnPatch(const Vector3D& coord, const Real& time)
 const bool nodeOnPatchCurrent(const Vector3D& coord, const Real& time)
 {
     bool nodeInArea = 0;
-
+    std::cout<<"nodeOnPatchCurrent:Skalarprodukt="<<(normal_vector[0]*coord[0] + normal_vector[1]*coord[1] + normal_vector[2]*coord[2] - normal_vector[0]*starting_point[0]- normal_vector[1]*starting_point[1]-normal_vector[2]*starting_point[2]);
+    
     //as shift we had + 0.35
         if((normal_vector[0]*coord[0] + normal_vector[1]*coord[1] + normal_vector[2]*coord[2] - normal_vector[0]*starting_point[0]- normal_vector[1]*starting_point[1]-normal_vector[2]*starting_point[2]) <= 0)
         {
