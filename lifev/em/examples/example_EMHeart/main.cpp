a6cb925fc62ccaff7d51acd16a0fef2ad41de9fb
L G <glaus.luaks@gmail.com>
2020-02-23 22:16:31 +0100

    rp 1.8.0

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -698,7 +698,7 @@ int main (int argc, char** argv)
         
         for (int k (0); k <= simple_iterations-1; k++) // here begins the time looping
         {
-            
+            //8.1.0
             pseudotime=(m_tduration)*(k/simple_iterations);
             if ( 0 == comm->MyPID() )
                 {
