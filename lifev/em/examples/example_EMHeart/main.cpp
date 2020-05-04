29a99bb8e02c543c09b06b10fada0284060bb916
L G <glaus.luaks@gmail.com>
2020-05-04 09:25:40 +0200

    rp 2.3.13

--- lifev/em/examples/example_EMHeart/main.cpp
+++ lifev/em/examples/example_EMHeart/main.cpp
@@ -854,7 +854,7 @@ int main (int argc, char** argv)
                     std::ostringstream timestream;//this is to convert int to string
                     timestream << t<<"_";
                     //std::string path = "/cluster/home/lglaus/LIFE5/lifev-env-rp/lifev-em-install/lifev/em/examples/example_EMHeart/volumefiles/"+timestream.str() +"volume"+ ".dat"; //2020.02.05 lg
-                    std::string path = problemFolder+timestream.str() + "volume" + ".dat"; //2020.04.28 lg, unfortunately doesnt work because problemFolder isnt available in this class, it is only in main. there would be a function required in main which fed the problemFolder information into the patch classes
+                    std::string path = problemFolder + "/volume/volume_file.dat"; //2020.04.28 lg, unfortunately doesnt work because problemFolder isnt available in this class, it is only in main. there would be a function required in main which fed the problemFolder information into the patch classes
                     
 
                     std::ofstream writer(path.c_str(), std::ios_base::app);
